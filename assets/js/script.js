6c191ac7f357dd08333722882dbf2c02e3d03a1e
taimour-hssn1 <bcsf22m504@pucit.edu.pk>
2025-08-14 02:29:18 -0700

    subjectify img issue

--- assets/js/script.js
+++ assets/js/script.js
@@ -98,7 +98,7 @@ function showProjects(projects) {
         const btns = `${viewBtn}${codeBtn}`;
         projectHTML += `
         <div class="box tilt">
-      <img draggable="false" src="/assets/images/projects/${project.image}.png" alt="project" />
+      <img draggable="false" src="./assets/images/projects/${project.image}.png" alt="project" />
       <div class="content">
         <div class="tag">
         <h3>${project.name}</h3>
