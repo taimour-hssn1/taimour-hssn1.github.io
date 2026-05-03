db12efc55ecb577d8db9213a267890d3c2ade235
taimour-hssn1 <taimourh05@gmail.com>
2026-05-03 10:25:03 +0500

    Navbar and about section updated

---
+++ src/components/About.tsx
@@ -0,0 +1,58 @@
+import React from "react";
+import "../styles/About.css";
+import FadeInSection from "./FadeInSection";
+
+const About = () => {
+  const one = (
+    <p>
+      I am currently an <b>Associate Software Engineer</b> at
+      <a href="https://infotechgroup.com/"> InfoTech</a>, where I help build and bug fix
+      our fintech platform. Previously, I was at{" "}
+      <a href="https://zendevco.com/"> Zendev</a> working as a Python Developer.
+    </p>
+  );
+  const two = (
+    <p>
+      In my free time, I’m nerdy about tech learning new concepts, love building and breaking things in loop, and 
+      sometimes go explore Generative AI concepts and learn some of them that fascinates me more.
+    </p>
+  );
+
+  const techStack = [
+    "Python",
+    "Typescript",
+    "React.js",
+    "Java",
+    "Javascript ES6+",
+    "C++",
+  ];
+
+  return (
+    <div id="about">
+      <FadeInSection>
+        <div className="section-header ">
+          <span className="section-title">/ about me</span>
+        </div>
+        <div className="about-content">
+          <div className="about-description">
+            {one}
+            {"Here are some technologies I have been working with:"}
+            <ul className="tech-stack">
+              {techStack.map((techItem, i) => (
+                <FadeInSection key={i} delay={(i + 1) * 100 + "ms"}>
+                  <li>{techItem}</li>
+                </FadeInSection>
+              ))}
+            </ul>
+            {two}
+          </div>
+          <div className="about-image">
+            <img alt="taimour" src={"/assets/taimour.png"} />
+          </div>
+        </div>
+      </FadeInSection>
+    </div>
+  );
+};
+
+export default About;
